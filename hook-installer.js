99f293759c0647ab6c2d2b1c881c3823fd199264
Meeusen <Meeusen.Christopher@mayo.edu>
2020-05-21 17:10:02 -0500

    added installer scripts scaffolding

---
+++ hook-installer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+// TODO hook install script here.
+// Control Flow:
+// Copy the contents of hook.js to .git/hooks/commit-msg
+// if error throw
+// else console.log('Successfully added hook')
+
---
+++ hook.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+// TODO implement the following control flow:
+// validate host platform is supported
+// Get the global npm registry location on the host machine
+// Determine host machine platform (ie windows, linux, or mac)
+// fspath.join to create the link to the platform specific binary
+// execute the binary providing the commit message file path, and the repo hooks path
