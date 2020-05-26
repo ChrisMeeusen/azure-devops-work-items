daee5cbef310a37e648561366898264f7dd15d8e
Meeusen <Meeusen.Christopher@mayo.edu>
2020-05-26 15:53:44 -0500

    .

--- package-script.js
+++ package-script.js
@@ -1,11 +1,14 @@
-'use strict';
 const fs = require('fs');
+const {google} = require('googleapis');
 
 /**
  *  The purpose of this script is to sync the pakage.json version across all 4 packages, core, win, mac, and linux
 **/
 
 const mainPkg = JSON.parse(fs.readFileSync('./package.json'));
+/*
+
+
 const macPkg = JSON.parse(fs.readFileSync('./package.mac.json'));
 const linuxPkg = JSON.parse(fs.readFileSync('./package.linux.json'));
 const winPkg = JSON.parse(fs.readFileSync('./package.win.json'));
@@ -17,3 +20,6 @@ winPkg.version = mainPkg.version;
 fs.writeFileSync('./package.mac.json', JSON.stringify(macPkg, null, 2));
 fs.writeFileSync('./package.linux.json', JSON.stringify(linuxPkg, null, 2));
 fs.writeFileSync('./package.win.json', JSON.stringify(winPkg, null, 2));
+
+*/
+
