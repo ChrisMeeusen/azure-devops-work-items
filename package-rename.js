4fbdd071e021087c182cbd2e1e3c99169d616ace
Meeusen <Meeusen.Christopher@mayo.edu>
2020-05-26 16:23:57 -0500

    .

--- package-rename.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const fs = require('fs');
-
-fs.renameSync('./package/mac/package.mac.json','./package/mac/package.json');
-fs.renameSync('./package/win/package.win.json','./package/win/package.json');
-fs.renameSync('./package/linux/package.linux.json','./package/linux/package.json');
