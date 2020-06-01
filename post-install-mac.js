cf1068e2cb19af906ea2f56c4aad3f6ba2605fa3
Meeusen <Meeusen.Christopher@mayo.edu>
2020-06-01 16:32:04 -0500

    .

--- post-install-mac.js
+++ post-install-mac.js
@@ -2,39 +2,34 @@
 const { exec } = require("child_process");
 const fspath = require('path');
 const fs = require('fs');
-
 let tarUnzipDir;
-
-exec('sudo npm root -g', (error, stdout, stderr) => {
+exec('npm root -g', (error, stdout, stderr) => {
     if( error || stderr){
         console.log("error determining npm global node_modules location", error || stderr);
     }
     if(stdout){
-        makeDir(stdout);
+        stdout= stdout.trim();
         tarUnzipDir = fspath.join(stdout,'azure-devops-work-items-mac','dist','mac');
+        makeDir(stdout);
     }
 });
-
 const makeDir = (_path) => {
     console.log('making directory to tar extract into');
-    fs.mkdirSync(tarUnzipDir, {recursive: true});
-    console.log('made: ', fspath.join(_path,'azure-devops-work-items-mac','dist','mac'));
-    unzipFile(_path);
-}
+    fs.mkdir(tarUnzipDir, {recursive: true}, (err)=> {
+        if(err) throw err;
 
+        console.log('made: ', fspath.join(_path,'azure-devops-work-items-mac','dist','mac'));
+        unzipFile(_path);
+    });
+}
 const unzipFile = (_path) => {
     const zipPath = fspath.join(_path, 'azure-devops-work-items-mac','dist','mac.tar.gz');
     const destPath = tarUnzipDir;
-
     console.log('extracting archive...');
-
-    exec(`sudo tar -xvzf ${zipPath} -C ${destPath}`, (error, stdout, stderr) => {
-        if( error || stderr){
-            console.log("error extracting archive", error || stderr);
+    exec(`tar -xvzf ${zipPath} -C ${destPath}`, (error, stdout, stderr) => {
+        if( error ){
+            console.log("error extracting archive", error );
         }
-        if(stdout){
-            console.log('archive extracted');
-        }
+        console.log('archive extracted');
     });
-
 }
